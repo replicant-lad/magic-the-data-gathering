206d0bfb46d5e213c07a3c2094892a9a2faa1ee0
dldexter <85723331+dldexter@users.noreply.github.com>
2024-03-28 21:06:10 -0600

    Hefty lambda overhaul; does a good amount of the data processing upfront so it can be more easily consumed by our db uptake later on.

--- lambda_function.py
+++ lambda_function.py
@@ -6,13 +6,46 @@ from datetime import datetime
 
 
 def lambda_handler(event, context):
-    '''
-    Retrieves the latest bulk data dump from Scryfall and uploads it to s3
-    '''
 
-    api_url = "https://api.scryfall.com/bulk-data"
+    api_url: str = "https://api.scryfall.com/bulk-data"
+    bulk_uri : str
+    target_keys : list = ["oracle_id","name","set","released_at","rarity","cmc","colors","type_line","power","toughness","legalities","prices"]
+
     http = urllib3.PoolManager()
 
+    def flatten_json(json_data):
+        """
+        Input: Multileveled json supplied by Scryfall
+
+        Flattens the legalities and prices of the card json for easier consumption, additionally converts the string array of colors into a simple comma delineated string.
+
+        Returns: Flattened json for eventual db consumption
+        """
+        flattened_data = []
+        for item in json_data:
+            flattened_item = {
+                "oracle_id": item["oracle_id"],
+                "name": item["name"],
+                "set": item["set"],
+                "released_at": item["released_at"],
+                "rarity": item["rarity"],
+                "cmc": item["cmc"],
+                "colors": ",".join(item.get("colors", [])),
+                "type_line": item["type_line"],
+                "standard_legal": item["legalities"]["standard"],
+                "pioneer_legal": item["legalities"]["pioneer"],
+                "modern_legal": item["legalities"]["modern"],
+                "pauper_legal": item["legalities"]["pauper"],
+                "vintage_legal": item["legalities"]["vintage"],
+                "commander_legal": item["legalities"]["commander"],
+                "power": item.get("power"),
+                "toughness": item.get("toughness"),
+                "prices_usd": item["prices"]["usd"]
+            }
+            flattened_data.append(flattened_item)
+        return flattened_data
+
+    # Scryfall supplies a URI to the actual bulk data, the following code retrieves that then queries for the json itself, and finally loads it.
     try:
         response = http.request("GET", api_url)
         response_json = json.loads(response.data)
@@ -20,29 +53,48 @@ def lambda_handler(event, context):
 
     except Exception as e:
         print(e)
-    
+        
     for i in response_json["data"]:
             if i["type"] == "oracle_cards":
-                 uri = i["download_uri"]
-    
+                bulk_uri = i["download_uri"]
+
     try:
-        download = http.request("GET", uri)
-        print("got the download object")
-        download_json = json.loads(download.data)
-        print("Successfully got latest bulk-data load")
+        data = http.request("GET", bulk_uri).data
+        loaded_json = json.loads(data)
+        
+        print("Successfully got latest bulk-data")
+
     except Exception as e:
-         print(e)
+        print(e)
+
+    # Filters the json down to the actual keys we want for the final db, and then runs the flattening function.
+
+    filtered_json = []
+    for card in loaded_json:
+        filtered_card = {}
+        for key in target_keys:
+            if key in card:
+                filtered_card[key] = card[key]
+        filtered_json.append(filtered_card)
 
 
-    s3 = boto3.client('s3')
-    print("Connected to aws")
-    bucket ='bulk-data-landing'
+    flattened_json = flatten_json(filtered_json)
 
-    fileName = 'dump_' + datetime.now().strftime("%d-%m-%y-%H:%M:%S")
 
-    print(f"Attempting to upload to {fileName}")
-    uploadByteStream = bytes(json.dumps(download_json).encode('UTF-8'))
+    # Uploads to the s3 bucket.
 
-    s3.put_object(Bucket=bucket, Key=fileName, Body=uploadByteStream)
+    s3_client = boto3.client('s3')
 
-    print("Upload to s3 complete")
+    bucket_name = 'magic-the-data-gathering'
+    key = 'landing/bulk_load.json'
+
+    s3_client.put_object(
+        Bucket=bucket_name,
+        Key=key,
+        Body=json.dumps(flattened_json, indent=4).encode('utf-8')
+    )
+
+    print(f"Bulk data file uploaded to S3 bucket: {bucket_name}/{key}")
+
+
+
